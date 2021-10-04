a9e917404b8089532fb8f9968f12019b766947dd
Wadaarik <nicolasboudier12@gmail.com>
2021-10-04 14:07:59 +0200

    init project

---
+++ backend/routes/test.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const postController = require('./../controllers/test')
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
+
+console.log("test route")
+router.post('/',postController.createPost)
+router.put('/:id',postController.updatePostId)
+router.delete('/:id',postController.deletePostId)
+router.get('/:id',auth,postController.getPostId)
+router.get('/',postController.getAllPost)
+
+module.exports = router
