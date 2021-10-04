0ea9d7de64f237289f5fa6903f4c9fe396c57add
Lilian Gautier <lggautier95480@gmail.com>
2021-10-04 15:41:17 +0200

    ajout routes

---
+++ frontend/src/routes.js
@@ -0,0 +1,60 @@
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import React from 'react';
+import './App.css';
+
+class Navbar extends React.Component {
+    render()  {
+      return  (
+        <BrowserRouter>
+          <div>
+            <div class="navbar">
+                <p><Link to="/">Home</Link></p>
+                <p><Link to="/about">About</Link></p>
+                <p><Link to="/topics">Topics</Link></p>
+            </div>
+  
+            <hr />
+            <div className="main-route-place">
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/topics" component={Topics} />
+            </div>
+          </div>
+        </BrowserRouter>
+      );
+    }
+  }
+
+  
+  class Home extends React.Component {
+  
+    render()  {
+      return (
+        <div>
+          <h2>Home</h2>
+        </div>
+      );
+    }
+  }
+  
+  class About  extends React.Component {
+    render() {
+      return (
+        <div>
+          <h2>About</h2>
+        </div>
+      );
+    }
+  }
+  
+  class Topics extends React.Component {
+    render() {
+      return (
+        <div>
+          <h2>Topics</h2>
+        </div>
+      );
+    }
+  }
+
+  export default Navbar;
