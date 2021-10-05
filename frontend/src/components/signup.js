54dd37f30829d21c992781245b639ac5246ef518
Loituma <27847873+Loituma@users.noreply.github.com>
2021-10-05 10:17:36 +0200

    signup page

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,24 +1,48 @@
 import '../App.css';
 import React from 'react';
+import axios from 'axios';
 
 class Signup extends React.Component {
+    state = {
+        pseudo: '',
+        email: '',
+        password: ''
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const pseudo = this.state.pseudo ;
+        const email = this.state.email ;
+        const password = this.state.password ;
+
+        axios.post(`http://localhost:2999/user/signup/`, {
+            pseudo,
+            email,
+            password
+        })
+    }
+
     render()  {
         return  (
             <div id="signup" className="signup">
                 
                 <h1>Créer un compte</h1>
-                <div class="form">
-                    <label for="pseudo">Pseudo : </label>
-                    <input name="pseudo" type="text" />
-                </div>
-                <div class="form">
-                    <label for="email">Email : </label>
-                    <input name="email" type="text" />
-                </div>
-                <div class="form">
-                    <label for="mdp">Mot de passe : </label>
-                    <input name="mdp" type="text" />
-                </div>
+                <form onSubmit={this.handleSubmit}>
+                    <div class="form">
+                        <label for="pseudo">Pseudo : </label>
+                        <input name="pseudo" type="text" onChange={(e) => this.setState({ pseudo: e.target.value })} />
+                    </div>
+                    <div class="form">
+                        <label for="email">Email : </label>
+                        <input name="email" type="text" onChange={(e) => this.setState({ email: e.target.value })} />
+                    </div>
+                    <div class="form">
+                        <label for="mdp">Mot de passe : </label>
+                        <input name="mdp" type="password" onChange={(e) => this.setState({ password: e.target.value })} />
+                    </div>
+                    <input type="submit" value='submit'/>
+                </form>
 
             </div>
         );
