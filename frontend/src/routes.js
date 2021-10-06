914f79a5f5c67a404cee568a716729a3eecd6a0f
Loituma <27847873+Loituma@users.noreply.github.com>
2021-10-06 15:25:59 +0200

    syntaxe

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link, Redirect } from "react-router-dom";
 import React, {useState} from 'react';
 import './App.css';
 import Table from "./components/table";
@@ -6,38 +6,35 @@ import logo from './loituma.gif';
 import Signup from "./components/signup";
 import Login from "./components/login";
 import Post from "./components/post";
-import {Redirect} from "react-router-dom";
-
 
 export default function Navbar() {
     const [logged, setLogged] = useState(localStorage.getItem('tokenUser') !== null ? true : false);
+
     return (
         <BrowserRouter>
-        <div>
-            <div className="navbar">
-                <Link className="logo" to="/"><img src={logo} alt="Sheesh"></img></Link>
-                <div className="links">
-                    <p><Link to="/about">A propos</Link></p>
-                    <p><Link to="/table">Table</Link></p>
-
-                    {logged ? (<p><Link to="/post">Post</Link></p>) : (<p><Link to="/signup">Sign Up</Link></p>)}
-                    {logged ? (<p><Link onClick={()=>setLogged(false)} to="/disconnect">Disconnect</Link></p>) : (<p><Link to="/login">Login</Link></p>)}
-
+            <div>
+                <div className="navbar">
+                    <Link className="logo" to="/"><img src={logo} alt="Sheesh"></img></Link>
+                    <div className="links">
+                        <p><Link to="/about">A propos</Link></p>
+                        <p><Link to="/table">Table</Link></p>
+                        {logged ? (<p><Link to="/post">Post</Link></p>) : (<p><Link to="/signup">Sign Up</Link></p>)}
+                        {logged ? (<p><Link onClick={()=>setLogged(false)} to="/disconnect">Disconnect</Link></p>) : (<p><Link to="/login">Login</Link></p>)}
+                    </div>
+                </div>
+                <div className="main-route-place">
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/table" component={Table} />
+                    <Route path="/post" component={Post} />
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/login">
+                        <Login callbackOnLog={()=>setLogged(true)} />
+                    </Route>
+                    <Route path="/disconnect" component={Disconnect} />
                 </div>
             </div>
-            <div className="main-route-place">
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/table" component={Table} />
-            <Route path="/post" component={Post} />
-            <Route path="/signup" component={Signup} />
-            <Route path="/login">
-                <Login callbackOnLog={()=>setLogged(true)} />
-            </Route>
-            <Route path="/disconnect" component={Disconnect} />                
-            </div>
-        </div>
-    </BrowserRouter>
+        </BrowserRouter>
     );
 }
 
