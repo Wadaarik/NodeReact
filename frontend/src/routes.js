2f7fcd85265ddbf4135407bcbd203c392e0a1360
Lilian Gautier <lggautier95480@gmail.com>
2021-10-06 11:56:11 +0200

    navbar update

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -6,32 +6,40 @@ import logo from './loituma.gif';
 import Signup from "./components/signup";
 import Login from "./components/login";
 import Post from "./components/post";
+import {Redirect} from "react-router-dom";
 
 class Navbar extends React.Component {
+
     render()  {
-      return  (
+
+        const logged = localStorage.getItem('tokenUser') !== null ? true : false
+
+        return (
         <BrowserRouter>
-          <div>
-            <div className="navbar">
-                <Link className="logo" to="/"><img src={logo} alt="Sheesh"></img></Link>
-                <div className="links">
-                    <p><Link to="/about">A propos</Link></p>
-                    <p><Link to="/table">Table</Link></p>
-                    <p><Link to="/signup">Sign Up</Link></p>
-                    <p><Link to="/login">Login</Link></p>
+            <div>
+                <div className="navbar">
+                    <Link className="logo" to="/"><img src={logo} alt="Sheesh"></img></Link>
+                    <div className="links">
+                        <p><Link to="/about">A propos</Link></p>
+                        <p><Link to="/table">Table</Link></p>
+
+                        {logged ? (<p><Link to="/post">Post</Link></p>) : (<p><Link to="/signup">Sign Up</Link></p>)}
+                        {logged ? (<p><Link to="/disconnect">Disconnect</Link></p>) : (<p><Link to="/login">Login</Link></p>)}
+
+                    </div>
+                </div>
+                <div className="main-route-place">
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/table" component={Table} />
+                <Route path="/post" component={Post} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route path="/disconnect" component={Disconnect} />                
                 </div>
             </div>
-            <div className="main-route-place">
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/table" component={Table} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/post" component={Post} />
-            </div>
-          </div>
         </BrowserRouter>
-      );
+        );
     }
   }
 
@@ -44,6 +52,15 @@ class Navbar extends React.Component {
           <h2>Home</h2>
         </div>
       );
+    }
+  }
+
+  class Disconnect extends React.Component {
+    render() {
+        localStorage.clear();
+        return (
+            <Redirect to="/" />
+        );
     }
   }
   
