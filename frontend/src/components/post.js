5d12c4159c51dfdff0eac3a0fc9d5cd1fa855af9
Lilian Gautier <lggautier95480@gmail.com>
2021-10-06 15:37:56 +0200

    Changement de poste

--- frontend/src/components/post.js
+++ frontend/src/components/post.js
@@ -1,22 +1,17 @@
 import '../App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-class Post extends React.Component {
+export default function Post() {
 
-    state = {
-        title: '',
-        text: '',
-        error: null,
-        success: null
-    }
+    const [titre, setTitre] = useState('');
+    const [text, setText] = useState('');
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
+    const autor = localStorage.getItem('pseudoUser') ;
 
-    handleSubmit = event => {
+    function handleSubmit(event) {
         event.preventDefault();
-
-        const autor = localStorage.getItem('pseudoUser') ;
-        const titre = this.state.title ;
-        const text = this.state.text ;
 
         axios.post(`http://localhost:2999/blog/`, {
             autor,
@@ -24,39 +19,35 @@ class Post extends React.Component {
             text
         }).then(res => {
             if (res.status === 201) {
-                this.setState({success: res.data.success});
-                this.setState({error: null});
+                setSuccess(success.res.data.success);
+                setError(error.null);
             }
         }).catch(error => {
             if (error.response) {
-                this.setState({error: error.response.data.error});
-                this.setState({success: null});
+                setError(error.response.data.error);
+                setSuccess(success.null);
             }
         });
     }
 
-    render()  {
-        return  (
-            <div id="post" className="post">
-                <div>
-                    <form id="form" onSubmit={this.handleSubmit}>
-                        <h1>Création de post</h1>
-                        {this.state.success ? <p className="msgSuccess">{this.state.success}</p> : ''}
-                        {this.state.error ? <p className="msgError">{this.state.error}</p> : ''}
-                        <div className="form">
-                            <label htmlFor="title">Titre : </label>
-                            <input name="title" type="text" onChange={(e) => this.setState({ title: e.target.value })} />
-                        </div>
-                        <div className="form">
-                            <label htmlFor="text">Texte : </label>
-                            <textarea name="text" rows="5" onChange={(e) => this.setState({ text: e.target.value })} />
-                        </div>
-                        <button type="submit" className="submit">Poster son texte</button>
-                    </form>
-                </div>
+    return  (
+        <div id="post" className="post">
+            <div>
+                <form id="form" onSubmit={handleSubmit}>
+                    <h1>Création de post</h1>
+                    {success ? <p className="msgSuccess">{success}</p> : ''}
+                    {error ? <p className="msgError">{error}</p> : ''}
+                    <div className="form">
+                        <label htmlFor="title">Titre : </label>
+                        <input name="title" type="text" onChange={(e) => setTitre(e.target.value)} />
+                    </div>
+                    <div className="form">
+                        <label htmlFor="text">Texte : </label>
+                        <textarea name="text" rows="5" onChange={(e) => setText(e.target.value)} />
+                    </div>
+                    <button type="submit" className="submit">Poster son texte</button>
+                </form>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default Post;
